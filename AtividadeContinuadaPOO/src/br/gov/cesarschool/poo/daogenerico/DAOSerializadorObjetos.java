8cf9f6535010702d176570d47ead0a8ed5b192a3
FelipeARFranca <feliperomeirofranca@gmail.com>
2024-11-22 13:16:16 -0300

    DAO funcional
    DAO funcional e passando em todos os testes da primeira bateria de  testes

--- AtividadeContinuadaPOO/src/br/gov/cesarschool/poo/daogenerico/DAOSerializadorObjetos.java
+++ AtividadeContinuadaPOO/src/br/gov/cesarschool/poo/daogenerico/DAOSerializadorObjetos.java
@@ -1,4 +1,15 @@
 package br.gov.cesarschool.poo.daogenerico;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 /*
  * Esta classe representa um DAO genérico, que inclui, exclui, altera, busca por identificador 
  * único e busca todos, qualquer objeto(s) cujo tipo é subtipo de Entidade.
@@ -14,15 +25,136 @@ package br.gov.cesarschool.poo.daogenerico;
 public class DAOSerializadorObjetos {
 	private String nomeDiretorio; 
 	public DAOSerializadorObjetos(Class<?> tipoEntidade) {
-	}	
+		this.nomeDiretorio = "." + File.separator + tipoEntidade.getSimpleName();
+	}
 	public boolean incluir(Entidade entidade) {
+		File diretorio = new File(nomeDiretorio + File.separator);
+		
+		if(!diretorio.exists()) {
+			diretorio.mkdirs();
+		}
+		
+		File arquivo = new File(nomeDiretorio + File.separator + entidade.getIdUnico());
+		
+		if (arquivo.exists()) {
+			return false;
+		}
+		else {
+			entidade.setDataHoraInclusao(LocalDateTime.now());
+			try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(arquivo))) {
+	            oos.writeObject(entidade);
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	            return false;
+	        }
+			
+			return true;
+		}
 	}
 	public boolean alterar(Entidade entidade) {
+		
+		File diretorio = new File(nomeDiretorio + File.separator);
+		
+		if(!diretorio.exists()) {
+			diretorio.mkdirs();
+		}
+		
+		File arquivo = new File(nomeDiretorio + File.separator + entidade.getIdUnico());
+		
+		if (!arquivo.exists()) {
+			return false;
+		}
+		else {
+			arquivo.delete();
+			
+			arquivo = new File(nomeDiretorio + File.separator + entidade.getIdUnico());
+			entidade.setDataHoraUltimaAlteracao(LocalDateTime.now());
+			
+			try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(arquivo))) {
+	            oos.writeObject(entidade);
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	            return false;
+	        }
+			
+			return true;
+		}
 	}
 	public boolean excluir(String idUnico) {
+		
+		File diretorio = new File(nomeDiretorio + File.separator);
+		
+		if(!diretorio.exists()) {
+			diretorio.mkdirs();
+		}
+		
+		File arquivo = new File(nomeDiretorio + File.separator + idUnico);
+		
+		if (!arquivo.exists()) {
+			return false;
+		}
+		else {
+			arquivo.delete();
+			return true;
+		}
 	}
 	public Entidade buscar(String idUnico) {
+		
+		File diretorio = new File(nomeDiretorio + File.separator);
+		
+		if(!diretorio.exists()) {
+			diretorio.mkdirs();
+		}
+		
+		File arquivo = new File(nomeDiretorio + File.separator + idUnico);
+		
+		if (!arquivo.exists()) {
+			return null;
+		}
+		else {
+		
+			Entidade entidade = null;
+			
+			try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(arquivo))) {
+	            entidade = (Entidade) ois.readObject();
+	        } catch (IOException | ClassNotFoundException e) {
+	            e.printStackTrace();
+	            return null;
+	        }
+			
+			return entidade;
+		}
 	}
 	public Entidade[] buscarTodos() {
+		
+		File diretorio = new File(nomeDiretorio + File.separator);
+		
+		if(!diretorio.exists()) {
+			diretorio.mkdirs();
+		}
+		
+		List<Entidade> entidadesList = new ArrayList<>();
+		
+		Entidade [] entidades = new Entidade[0];
+		Entidade entidade = null;
+		
+		File [] arquivos = diretorio.listFiles();
+		
+		if (arquivos != null) {
+			for (File arquivo : arquivos) {
+				try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(arquivo))) {
+		            entidade = (Entidade) ois.readObject();
+		        } catch (IOException | ClassNotFoundException e) {
+		            e.printStackTrace();
+		        }
+				entidadesList.add(entidade);
+			}
+			
+			return entidadesList.toArray(new Entidade[0]);
+		}
+		else {
+			return entidades;
+		}
+		
 	}
 }
--- AtividadeContinuadaPOO/src/br/gov/cesarschool/poo/daogenerico/Entidade.java
+++ AtividadeContinuadaPOO/src/br/gov/cesarschool/poo/daogenerico/Entidade.java
@@ -1,4 +1,8 @@
 package br.gov.cesarschool.poo.daogenerico;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
 /*
  * Esta classe representa uma superclasse de todas as entidades. 
  * 
@@ -16,5 +20,49 @@ package br.gov.cesarschool.poo.daogenerico;
  * 
  * Deve implementar a interface Serializable do JAVA
  */
-public abstract class Entidade {	
+public abstract class Entidade implements Serializable {	
+	private LocalDateTime dataHoraInclusao;
+	private LocalDateTime dataHoraUltimaAlteracao;
+	private String usuarioInclusao;
+	private String usuarioUltimaAlteracao;
+	
+	public Entidade() {
+		
+	}
+	
+	public abstract String getIdUnico();
+
+	public LocalDateTime getDataHoraInclusao() {
+		return dataHoraInclusao;
+	}
+
+	public void setDataHoraInclusao(LocalDateTime dataHoraInclusao) {
+		this.dataHoraInclusao = dataHoraInclusao;
+	}
+
+	public LocalDateTime getDataHoraUltimaAlteracao() {
+		return dataHoraUltimaAlteracao;
+	}
+
+	public void setDataHoraUltimaAlteracao(LocalDateTime dataHoraUltimaAlteracao) {
+		this.dataHoraUltimaAlteracao = dataHoraUltimaAlteracao;
+	}
+
+	public String getUsuarioInclusao() {
+		return usuarioInclusao;
+	}
+
+	public void setUsuarioInclusao(String usuarioInclusao) {
+		this.usuarioInclusao = usuarioInclusao;
+	}
+
+	public String getUsuarioUltimaAlteracao() {
+		return usuarioUltimaAlteracao;
+	}
+
+	public void setUsuarioUltimaAlteracao(String usuarioUltimaAlteracao) {
+		this.usuarioUltimaAlteracao = usuarioUltimaAlteracao;
+	}
+	
+	
 }
